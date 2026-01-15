7970e313048a08c529d7a9b0ebfd7cef23ab7fb1
abdulxrahaman <muhammedabdul7860@gmail.com>
2026-01-15 11:24:00 +0530

    Basic mathematical operations

---
+++ Assign1.py
@@ -0,0 +1,12 @@
+#Task-1
+'''Perform basic mathematical operation'''
+a = int(input('Enter the first number: '))
+b = int(input('Enter the second number: '))
+add = a+b
+sub = a-b
+mul = a*b
+div = a/b
+print('Additon: ',add)
+print('Subtraction: ',sub)
+print('Multiplication: ',mul)
+print('Division: ',div)
